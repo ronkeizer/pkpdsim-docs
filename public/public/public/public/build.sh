02518f7c6cd2dcf5bb68bd4f50a9042ba9d6af79
ronkeizer <ronkeizer@gmail.com>
2017-03-31 14:38:40 -0700

    updates

--- public/public/public/public/build.sh
+++
@@ -1,6 +0,0 @@
-# build md into html in _book folder
-gitbook build
-
-# for some reason Heroku doesn't like the _book folder...
-rm -rf public
-cp -R _book public
--- public/public/public/public/license.md
+++
@@ -1,3 +0,0 @@
-# License
-
-`PKPDsim` is released under the [MIT open source license](http://opensource.org/licenses/MIT).
